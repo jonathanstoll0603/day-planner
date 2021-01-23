75656b7d503d629c1b9747629a46ecf64d54c136
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-01-23 12:55:27 -0500

    local storage now displaying text on page after refresh

--- assets/main.js
+++ assets/main.js
@@ -3,23 +3,28 @@ $(document).ready(function () {
 // now variable returns formatted date in Jan-Dec 01-31 20XX
 var nowFormatted = dayjs().format("MMM DD YYYY");
 
-var currentTimeHeader = dayjs().format("h:mm A") // returns header clock time in XX:XX AM/PM
-var currentTime = dayjs().format("h:mm"); // returns current time in XX:XX 
-
-var currentTimeHour = dayjs().format("h"); // Turns current time to number
-console.log(currentTime)
-console.log(currentTimeHour)
+// returns header clock time in XX:XX AM/PM
+var currentTimeHeader = dayjs().format("h:mm A") 
 console.log(dayjs())
 
-var workdayHoursArray = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM']; // Array containing all the hours in workday. Used to create the number of rows and display time.
-var workdayHoursMilitary = [9, 10, 11, 12, 13, 14, 15, 16, 17]; // array containing workday in military time to compare in the checkTime function
+// Array containing all the hours in workday. Used to create the number of rows and display time.
+var workdayHoursArray = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM']; 
 
-$("#currentDay").text(currentTimeHeader + " on: " + nowFormatted); // currentDay div text
+// Array containing all workday ours in military time. Used to compare to actual time and show past/present/future
+var workdayHoursMilitaryTimeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
+ // currentDay div text
+$("#currentDay").text(currentTimeHeader + " on: " + nowFormatted);
 
+// arrays containing saved user information to be called for local storage storage.
+var todoTextArr = [];
+var todoTimeArr = [];
+
+// Function that dynamically creates the html content and attributes
 function createTable() {
     
-    var container = $(".container"); // variable for main content container
+    // variable for main content container
+    var container = $(".container"); 
 
     // repeat this function for a total of 8 times (num hours in workday)
     for (var i = 0; i < workdayHoursArray.length; i++) {
@@ -27,8 +32,7 @@ function createTable() {
         // Each row's div container
         var divRow = $("<div>").addClass("row time-block").appendTo(container);
         // Adding ID attribute to each row to parse the int in the checkTime func
-        divRow.attr("id", (workdayHoursMilitary[i]))
-        console.log(divRow.attr("id"))
+        divRow.attr("id", (workdayHoursMilitaryTimeArray[i]));
 
         // Hour count div on left hand side of row
         var timeDiv = $("<div>").addClass("col-md-1 hour").appendTo(divRow); 
@@ -36,20 +40,23 @@ function createTable() {
 
         // Textarea in middle of row
         var textAreaInput = $("<textarea>").addClass("col-md-10 description").appendTo(divRow);
-        textAreaInput.text("");
+        textAreaInput.attr("id", "description" + workdayHoursMilitaryTimeArray[i]);
 
         // Save button on right hand side of row
         var saveButton = $("<button>").addClass("col-md-1 saveBtn").appendTo(divRow);
+
         saveButton.attr("type", "submit");
         // button image found on button
         var buttonImg = $("<i>").addClass("fas fa-save").appendTo(saveButton);
     }
-
 }
 createTable() // Calling create table when page first loads
 
 // Function used to check current time and color each row depending on past, present, or future
 function checkTime() {
+
+    var currentTimeHour = dayjs().format("H"); // Turns current time to number
+
     // Checks each time-block div 
     $(".time-block").each(function() {
         // variable to parse the int of the time block ID
@@ -62,7 +69,7 @@ function checkTime() {
             $(this).addClass("future");
 
         // If current time equals the row time
-        } else if (timeParse === workdayHoursMilitary) {
+        } else if (timeParse == currentTimeHour) {
             $(this).removeClass("past");
             $(this).removeClass("future");
             $(this).addClass("present");
@@ -77,22 +84,45 @@ function checkTime() {
 }
 checkTime() //Calls checkTime function
 
+// function below is called and run when the page loads
+function init() {
+    retrieveTodos();
+}
+
+// function that retrieves data from localStorage
+function retrieveTodos() {
+
+    // retrieves our stored text and stored time from localStorage and display it onto our page.
+    $("#description9").val(localStorage.getItem("description9"));
+    $("#description10").val(localStorage.getItem("description10"));
+    $("#description11").val(localStorage.getItem("description11"));
+    $("#description12").val(localStorage.getItem("description12"));
+    $("#description13").val(localStorage.getItem("description13"));
+    $("#description14").val(localStorage.getItem("description14"));
+    $("#description15").val(localStorage.getItem("description15"));
+    $("#description16").val(localStorage.getItem("description16"));
+    $("#description17").val(localStorage.getItem("description17"));
+
+}
+
 // Button click event for the save button
 $(".saveBtn").on("click", function(event) {
     event.preventDefault();
-
-    // stores the values of the user's text area input into variable
+    // stores the values of the user's text area input and it's ID into variables 
     var todoText = $(this).siblings(".description").val();
-    var todoTime = $(this).parent().attr("id");
+    var todoTime = $(this).siblings(".description").attr("id");
 
+    // if there is no text entered when user saves, do nothing.
     if (todoText === '') {
         return;
     }
 
-    localStorage.setItem(todoText, todoTime);
-    console.log(todoText, todoTime);
+    // Stores todoTime as the key, todoText as the value
+    localStorage.setItem(todoTime, todoText)
+    retrieveTodos();
 })
 
-$("#9 .description").val(localStorage.getItem("9"));
+// Calls init function on page load
+init();
 
 })    
